b1ac851473b57a44e15de46946ce57e35bb22c36
Serius69 <seriogionis@gmail.com>
2022-05-11 11:29:58 -0400

    [DEV] Dependencias

---
+++ src/test/java/ucb/edu/arqsoft/mscheckout/MsCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.edu.arqsoft.mscheckout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
